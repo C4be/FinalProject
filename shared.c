a9069dc894b29a32018d7ffe6de4e5bab4ea6043
lifefire1 <firend228@mail.ru>
2023-07-13 02:57:54 +0300

    add select logic

--- shared.c
+++ shared.c
@@ -1,1 +1,99 @@
 #include "shared.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "struct/modules.h"
+//////////////////////////////////////////////////////
+int get_file_size_in_bytes(FILE *pfile) {
+    int size;
+    fseek(pfile, 0, SEEK_END);
+    size = ftell(pfile);
+    rewind(pfile);
+    return size;
+}
+
+
+int get_records_count_in_file(FILE *pfile) {
+    return get_file_size_in_bytes(pfile) / sizeof(modules);
+}
+
+int select_modules_by(FILE *db, const char *field, int value) {
+    int struct_count = get_records_count_in_file(db);
+    modules *modules1 = (modules *)malloc(sizeof(modules));
+    printf("--------------------------------------------------------------------------------------------------------------------------\n");
+    printf("|                                                  MODULES_TABLE                                                        |\n");
+    printf("--------------------------------------------------------------------------------------------------------------------------\n");
+    printf("|\tid\t|\tmodule_name\t|\tlevel_memory_number\t|\tceil_number\t|\tflag_delete\t|\n");
+    printf("--------------------------------------------------------------------------------------------------------------------------\n");
+    for (int i = 0; i < struct_count; ++i) {
+        fread(modules1, 1, sizeof(modules), db);
+        if (strcmp(field, "id") == 0 && modules1->id == value) {
+            printf("|%14d | %22s | %29d | %21d | %22d|\n",
+                   modules1->id,
+                   modules1->module_name,
+                   modules1->level_memory_number,
+                   modules1->ceil_number,
+                   modules1->flag_delete
+            );
+        } else if (strcmp(field, "level_memory_number") == 0 && modules1->level_memory_number == value) {
+            printf("|%14d | %22s | %29d | %21d | %22d|\n",
+                   modules1->id,
+                   modules1->module_name,
+                   modules1->level_memory_number,
+                   modules1->ceil_number,
+                   modules1->flag_delete
+            );
+        } else if (strcmp(field, "ceil_number") == 0 && modules1->ceil_number == value) {
+            printf("|%14d | %22s | %29d | %21d | %22d|\n",
+                   modules1->id,
+                   modules1->module_name,
+                   modules1->level_memory_number,
+                   modules1->ceil_number,
+                   modules1->flag_delete
+            );
+        } else if (strcmp(field, "flag_delete") == 0 && modules1->flag_delete == value) {
+            printf("|%14d | %22s | %29d | %21d | %22d|\n",
+                   modules1->id,
+                   modules1->module_name,
+                   modules1->level_memory_number,
+                   modules1->ceil_number,
+                   modules1->flag_delete
+            );
+        }
+        printf("--------------------------------------------------------------------------------------------------------------------------\n");
+    }
+
+    free(modules1);
+    return 0;
+}
+
+int select_modules_by_module_name(FILE *db, char *value){
+    int struct_count = get_records_count_in_file(db);
+    modules *modules1 = (modules *)malloc(sizeof(modules));
+    printf("--------------------------------------------------------------------------------------------------------------------------\n");
+    printf("|                                                  MODULES_TABLE                                                        |\n");
+    printf("--------------------------------------------------------------------------------------------------------------------------\n");
+    printf("|\tid\t|\tmodule_name\t|\tlevel_memory_number\t|\tceil_number\t|\tflag_delete\t|\n");
+    printf("--------------------------------------------------------------------------------------------------------------------------\n");
+    for (int i = 0; i < struct_count; ++i) {
+        fread(modules1, 1, sizeof(modules), db);
+        if (strcmp(modules1->module_name,value) == 0) {
+            printf("|%14d | %22s | %29d | %21d | %22d|\n",
+                   modules1->id,
+                   modules1->module_name,
+                   modules1->level_memory_number,
+                   modules1->ceil_number,
+                   modules1->flag_delete
+            );
+        }
+        else{
+            printf("|               |                       |                               |                       |                       |\n");
+        }
+        printf("--------------------------------------------------------------------------------------------------------------------------\n");
+    }
+    return 0;
+}
+int main(){
+    select_modules_by_module_name(fopen("../materials/master_levels.db","r"),"");
+}
---
+++ struct/modules.h
@@ -0,0 +1,16 @@
+//
+// Created by lifefire1 on 13.07.23.
+//
+
+#ifndef FINALPROJECT_MODULES_H
+#define FINALPROJECT_MODULES_H
+
+
+typedef struct {
+    int id;
+    char module_name[30];
+    int level_memory_number;
+    int ceil_number;
+    int flag_delete;
+} modules;
+#endif  // FINALPROJECT_MODULES_H
