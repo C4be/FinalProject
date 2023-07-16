13d6d4506e7ba90190600d237d0c990abba08346
Alexey Iakhnenko <79930276+lifefire1@users.noreply.github.com>
2023-07-17 02:06:00 +0300

    Add files via upload
    penis

---
+++ cmake-build-debug/CMakeFiles/db.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/db.dir/core/crud/create_table.c.o"
+  "CMakeFiles/db.dir/core/crud/create_table.c.o.d"
+  "CMakeFiles/db.dir/core/db_manipulation/db_helper.c.o"
+  "CMakeFiles/db.dir/core/db_manipulation/db_helper.c.o.d"
+  "CMakeFiles/db.dir/core/db_manipulation/db_man.c.o"
+  "CMakeFiles/db.dir/core/db_manipulation/db_man.c.o.d"
+  "CMakeFiles/db.dir/core/global_var.c.o"
+  "CMakeFiles/db.dir/core/global_var.c.o.d"
+  "db"
+  "db.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/db.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
